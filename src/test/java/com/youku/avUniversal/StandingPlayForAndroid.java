73d5f547cdd47b4b27b85d9d6e70fdb2b7eb04b8
庭婳 <meifang.ymf@alibaba-inc.com>
2021-09-28 10:57:50 +0800

    fix

--- src/test/java/com/youku/avUniversal/StandingPlayForAndroid.java
+++ src/test/java/com/youku/avUniversal/StandingPlayForAndroid.java
@@ -24,7 +24,7 @@ public class StandingPlayForAndroid extends PlayerBaseCase {
         CmdExecutor cmdExecutor = new CmdExecutor();
         // -s 指定不同手机
         String time = new SimpleDateFormat( "yyyyMMddHHmmssSSS" ).format( new Date() );
-        String cmd = String.format( "scrcpy%s--max-fps 60 --bit-rate 2M --max-size 1080 -Nr %s.mp4", deviceId, time );
+        String cmd = String.format( "scrcpy%s--max-fps 60 --bit-rate 2M --max-size 1080 -Nr /Users/yktest/av-test/record/%s.mp4", deviceId, time );
         logger.warn( "命令:" + cmd );
 
         try {
